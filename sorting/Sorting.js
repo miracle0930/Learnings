69d7f6de2c9ae6f45ce01f2671d210b69909bda9
Hao Guan <guanhao0930@nextdish.com>
2020-02-19 14:47:49 -0800

    complete heapsort and radixsort

--- sorting/Sorting.js
+++ sorting/Sorting.js
@@ -1,7 +1,7 @@
 const performance = require('perf_hooks').performance;
 
-let length = 10000;
-let seedLength = 1000000;
+let length = 1000000;
+let seedLength = 100000;
 
 const data = Array.from({ length }, () => Math.floor(Math.random() * seedLength));
 const answer = data.slice(0).sort((a, b) => a - b);
@@ -173,9 +173,78 @@ const mergeSort = (input) => {
   checkResult(arr, 'Merge sort', start, end);
 }
 
+const heapSort = (input) => {
+  const start = performance.now();
+  const arr = input.slice(0);
+  
+  const maxHeapify = (index, length) => {
+    const left = index * 2 + 1;
+    const right = index * 2 + 2;
+    let maxChild = left;
+    if (left > length) {
+      return;
+    }
+    if (right <= length && arr[right] > arr[left]) {
+      maxChild = right;
+    }
+    if (arr[maxChild] > arr[index]) {
+      swap(maxChild, index);
+      maxHeapify(maxChild, length);
+    }
+
+  }
+
+  const swap = (left, right) => {
+    const temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+  }
+
+  for (let i = parseInt(arr.length / 2) - 1; i >= 0; i -= 1) {
+    maxHeapify(i, arr.length);
+  }
+
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    swap(0, i);
+    maxHeapify(0, i - 1);
+  }
+
+  const end = performance.now();
+  checkResult(arr, 'Heap sort', start, end);
+}
+
+const radixSort = (input) => {
+  const start = performance.now();
+  const arr = input.slice(0);
+  const buckets = [];
+
+  for (let i = 0; i < input.length; i += 1) {
+    if (!buckets[arr[i]]) {
+      buckets[arr[i]] = 0;
+    }
+    buckets[arr[i]] += 1;
+  }
+
+  let pointer = 0;
+
+  for (let i = 0; i < buckets.length; i += 1) {
+    if (buckets[i] > 0) {
+      while (buckets[i] > 0) {
+        arr[pointer++] = i;
+        buckets[i] -= 1;
+      }
+    }
+  }
+
+  const end = performance.now();
+  checkResult(arr, 'Radix sort', start, end);
+}
+
 
 // selectionSort(data);
 // bubbleSort(data);
 // insertionSort(data);
-quickSort(data);
-mergeSort(data);
+// quickSort(data);
+// mergeSort(data);
+heapSort(data);
+radixSort(data);
